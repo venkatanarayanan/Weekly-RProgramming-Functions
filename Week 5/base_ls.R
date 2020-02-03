ba6db0f0a040424ba5274f285e1e8786399bb244
venkatanarayanan <venkatanarayanan22@gmail.com>
2020-02-03 10:38:29 +0530

    Week 5 - sprintf() and ls()

---
+++ Week 5/base_ls.R
@@ -0,0 +1,2 @@
+# Use ls() to print the variables in the current environment
+ls()
---
+++ Week 5/base_sprintf.R
@@ -0,0 +1,8 @@
+name <- "John"
+age <- 25
+
+# Use paste to combine strings and variables
+paste(name, "is", age, "years old.")
+
+# Use sprintf to combine strings and variables
+sprintf("%s is %d years old", name, age)
