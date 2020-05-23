28b31a87774f73e03117e95eaf64b2f73a4d00f8
venkatanarayanan <venkatanarayanan22@gmail.com>
2020-05-23 16:07:26 +0530

    Week 19 - ntile() and percent_rank()

---
+++ Week 19/dplyr_ntile.R
@@ -0,0 +1,10 @@
+# attach the 'dplyr' package
+library(dplyr)
+
+# Create a sample vector
+numbers_vector <- c(19, 16, 8, 22, 41, 3)
+
+# use the 'ntile' function to rank the elements of the vectors
+ntile(numbers_vector, 2)
+
+
