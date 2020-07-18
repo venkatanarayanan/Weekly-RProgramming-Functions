94f10a2f0cc2391353783619cae06523e4f148a4
venkatanarayanan <venkatanarayanan22@gmail.com>
2020-07-18 21:52:12 +0530

    tolower() and toupper()

---
+++ Week 27/base_tolower.R
@@ -0,0 +1,11 @@
+# Create a sample string
+sample_string <- "THIS IS MY NAME"
+
+# Use 'tolower' to convert the string to lower case
+tolower(sample_string)
+
+# Create another sample string
+another_string <- "This Is My NaMe"
+
+# Use 'tolower' to convert the string to lower case
+tolower(another_string)
---
+++ Week 27/base_toupper.R
@@ -0,0 +1,11 @@
+# Create a sample string
+sample_string <- "this is my name"
+
+# Use 'toupper' to convert the string to upper case
+toupper(sample_string)
+
+# Create another sample string
+another_string <- "THIS is MY name"
+
+# Use 'toupper' to convert the string to upper case
+toupper(another_string)
