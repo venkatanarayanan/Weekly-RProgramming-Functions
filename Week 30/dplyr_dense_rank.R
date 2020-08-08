79701590a705a298182fb583ba35be13c17538f3
unknown <venkatanarayanan22@gmail.com>
2020-08-08 22:01:30 +0530

    Week 30 - dense_rank() and row_number

---
+++ Week 30/dplyr_dense_rank.R
@@ -0,0 +1,8 @@
+# attach the 'dplyr' package
+library(dplyr)  
+
+# Create a sample vector
+x <- c(10, 2, 6, 6, 9)
+
+# use 'dense_rank' function to rank the elements of 'x'
+dense_rank(x)
---
+++ Week 30/dplyr_row_number.R
@@ -0,0 +1,8 @@
+# attach the 'dplyr' package
+library(dplyr)  
+
+# Create a sample vector
+x <- c(10, 2, 6, 6, 9)
+
+# use 'row_number' function to rank the elements of 'x'
+row_number(x)
