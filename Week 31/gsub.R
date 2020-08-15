f262671a4510f301ef2058466c673d6fbd3820cc
unknown <venkatanarayanan22@gmail.com>
2020-08-15 23:49:30 +0530

    Week 31 - gsub() and sub()

---
+++ Week 31/gsub.R
@@ -0,0 +1,8 @@
+# Create a sample string
+sample_string <- "This is a sample string. A SAMPLE STRING"
+
+# use the 'gsub' function to replace all instances of a character
+sample_string <- gsub("a", "c", sample_string)
+
+# print to check if the 'sample_string' is updated
+sample_string
---
+++ Week 31/sub.R
@@ -0,0 +1,8 @@
+# Create a sample string
+sample_string <- "This is a sample string"
+
+# use the 'sub' function to replace a character
+sample_string <- sub("a", "c", sample_string)
+
+# print to check if the 'sample_string' is updated
+sample_string
