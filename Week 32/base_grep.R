b61fa58e9330e37bf518a62d39539d73595c6283
unknown <venkatanarayanan22@gmail.com>
2020-08-23 00:00:22 +0530

    Week 32 - grep() and grepl()

---
+++ Week 32/base_grep.R
@@ -0,0 +1,8 @@
+# Create a sample vector
+sample_vec <- c("a", "c", "cc", "aaa")
+
+# use grep() to find out if the given pattern exists
+grep("aaa", sample_vec)
+
+# use grep() to find out if the given pattern exists
+grep("aaaa", sample_vec)
---
+++ Week 32/base_grepl.R
@@ -0,0 +1,8 @@
+# Create a sample vector
+sample_vec <- c("a", "c", "cc", "aaa")
+
+# use grepl() to find out if the given pattern exists
+grepl("aaa", sample_vec)
+
+# use grepl() to find out if the given pattern exists
+grepl("aaaa", sample_vec)
