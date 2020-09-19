4008fdb75c6aa788328f3ce2f77614fbbec2ee24
unknown <venkatanarayanan22@gmail.com>
2020-09-19 16:37:13 +0530

    Week 36 - paste() and paste0()

---
+++ Week 36/base_paste.R
@@ -0,0 +1,12 @@
+# Concatenate a sample sentence using paste
+sample_sentence <- paste("This", "is", "my", "name")
+
+# print the sentence to see how it has been concatenated
+sample_sentence
+
+# use a different seperator
+sample_sentence <- paste("This", "is", "my", "name", sep = "-")
+
+# print the sentence to see how it has been concatenated
+sample_sentence
+
---
+++ Week 36/base_paste0.R
@@ -0,0 +1,12 @@
+# Concatenate a sample sentence using paste0()
+sample_sentence <- paste0("This", "is", "my", "name")
+
+# print the sentence to see how it has been concatenated
+sample_sentence
+
+# The paste0() function has no seperator
+sample_sentence <- paste0("This", " ","is", " " ,"my", " ","name")
+
+# print the sentence to see how it has been concatenated
+sample_sentence
+
