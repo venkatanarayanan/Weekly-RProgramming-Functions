80a6f45520eb9fe50be5e295c222d978ff07fa3f
unknown <venkatanarayanan22@gmail.com>
2020-09-26 10:53:52 +0530

    Week 37 - type.convert and str_dup

---
+++ Week 37/stringr_str_dup.R
@@ -0,0 +1,8 @@
+# Load the 'stringr' package
+library(stringr)
+
+# create a sample string
+sample_string <- "What is your name?"
+
+# duplicate the string
+str_dup(sample_string, 2)
---
+++ Week 37/utlis_type.convert.R
@@ -0,0 +1,11 @@
+# create a sample vector
+numbers <- c("10", "11", "12", "13", "14", "15")
+
+# check the class of the sample vector
+class(numbers)
+
+# convert the type of the sample vector
+numbers <- type.convert(numbers)
+
+# check the class of the sample vector after conversion
+class(numbers)
