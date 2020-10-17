0566ca80f26977b65a77c9c21078ef79be3631c2
unknown <venkatanarayanan22@gmail.com>
2020-10-18 02:43:22 +0530

    Week 40 - pretty() and sample()

---
+++ Week 40/base_pretty.R
@@ -0,0 +1,5 @@
+# Create a sample vector
+num_vec <- c(1:200)
+
+# use pretty to return a sequence
+pretty(num_vec, n = 10)
---
+++ Week 40/base_sample.R
@@ -0,0 +1,8 @@
+# create a sample vector of numeric values
+num_vec <- c(11, 12, 13, 14, 15)
+
+# generate a random sub sample of the data
+sample(num_vec, size = 3)
+
+# generate a random sub sample of the data
+sample(num_vec, size = 3)
