2c242cd4863e041ee0521806d5682ee7ca8537e1
unknown <venkatanarayanan22@gmail.com>
2020-11-01 00:51:59 +0530

    Week 42 - pmax() and pmin()

---
+++ Week 42/rmpfr_pmax.R
@@ -0,0 +1,8 @@
+# Create a sample numeric vector
+num_vec_1 <- c(2,8,21,13,56)
+
+# Create another sample numeric vector
+num_vec_2 <- c(10, 26, 5, 38, 41)
+
+# Use pmax() to find the parallel maximum
+pmax(num_vec_1, num_vec_2)
---
+++ Week 42/rmpfr_pmin.R
@@ -0,0 +1,8 @@
+# Create a sample numeric vector
+num_vec_1 <- c(2,8,21,13,56)
+
+# Create another sample numeric vector
+num_vec_2 <- c(10, 26, 5, 38, 41)
+
+# Use pmin() to find the parallel maximum
+pmin(num_vec_1, num_vec_2)
