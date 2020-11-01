685ac5e998ebfc5bef146d2ef63d459b4d24351e
unknown <venkatanarayanan22@gmail.com>
2020-11-02 01:46:44 +0530

    Week 43 - rank() and order()

---
+++ Week 43/base_order.R
@@ -0,0 +1,5 @@
+# Create a sample numeric vector
+num_vec <- c(2,11,1,3)
+
+# Use order() to find the rank of each element in ascending order
+order(num_vec)
---
+++ Week 43/base_rank.R
@@ -0,0 +1,9 @@
+# Create a sample numeric vector
+num_vec <- c(2,11,1,3)
+
+# Use order() to find the rank of each element in ascending order
+order(num_vec)
+
+# Use order() to find the rank of each element in ascending order
+rank(num_vec)
+
