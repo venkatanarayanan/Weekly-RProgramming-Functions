bf34a71c359e4c346b0000fc56864f65b10d6557
unknown <venkatanarayanan22@gmail.com>
2020-11-21 19:58:50 +0530

    Week 45 - is.unsorted() and sort.int()

---
+++ Week 45/base_is_unsorted.R
@@ -0,0 +1,11 @@
+# Create a sample vector
+x <- c(6,3,8,1)
+
+# use is.unsorted() to check if the values are sorted
+is.unsorted(x)
+
+# Create a sample vector
+y <- c(1,2,3,5)
+
+# use is.unsorted() to check if the values are sorted
+is.unsorted(y)
---
+++ Week 45/base_sort_int.R
@@ -0,0 +1,11 @@
+# Create a sample vector
+x <- c(6,3,8,1)
+
+# use sort.int() to check if the values are sorted
+sort.int(x)
+
+# Create a sample vector
+y <- c(5,1,3,2)
+
+# use sort.int() to check if the values are sorted
+sort.int(y, decreasing = TRUE)
